461ac118d5f3bee24386027bed49b03c3a0dc2f0
Asad9340 <asadulimran1999@gmail.com>
2025-06-30 23:42:30 +0600

    api call updated

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,8 +1,5 @@
-'use client';
 
 const page = async () => {
-  const res = await fetch('http://localhost:3000/api/todos');
-  const data = await res.json();
   return (
     <div className="pt-20 max-w-6xl mx-auto px-4">
       <h1>{todo.todo.todos.length}</h1>
